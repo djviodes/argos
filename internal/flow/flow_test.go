c8f087e63fb83f0ac9051491981d6aad0ed88a71
David Viodes <djviodes26@gmail.com>
2026-08-14 00:05:34 -0700

    Add TestFlushAll and TestFlushAllCancelledCtx

--- internal/flow/flow_test.go
+++ internal/flow/flow_test.go
@@ -1,8 +1,10 @@
 package flow
 
 import (
+	"context"
 	"net"
 	"net/netip"
+	"sync"
 	"testing"
 	"time"
 )
@@ -96,7 +98,7 @@ func TestNewFlowKey(t *testing.T) {
 
 func TestAdd(t *testing.T) {
 	f := New()
-	firstSeenTiemstamp := time.Now().Add(-5 * time.Second)
+	firstSeenTimestamp := time.Now().Add(-5 * time.Second)
 	secondSeenTimestamp := time.Now()
 	pkt := fakePacket{
 		srcIP:     net.IP{0xc0, 0xa8, 0x01, 0x0a},
@@ -105,7 +107,7 @@ func TestAdd(t *testing.T) {
 		dstPort:   0x01bb,
 		protocol:  0x06,
 		length:    38,
-		timestamp: firstSeenTiemstamp,
+		timestamp: firstSeenTimestamp,
 	}
 	secondPkt := pkt
 	secondPkt.timestamp = secondSeenTimestamp
@@ -153,13 +155,163 @@ func TestAdd(t *testing.T) {
 				t.Errorf("got flow record packet count %#x, want flow record packet count %#x", f.flows[flowKey].packetCount, tt.wantPacketCount)
 			}
 
-			if f.flows[flowKey].firstSeen != firstSeenTiemstamp {
-				t.Errorf("got flow record first seen %v, want flow record first seen %v", f.flows[flowKey].firstSeen, firstSeenTiemstamp)
+			if f.flows[flowKey].firstSeen != firstSeenTimestamp {
+				t.Errorf("got flow record first seen %v, want flow record first seen %v", f.flows[flowKey].firstSeen, firstSeenTimestamp)
 			}
 
 			if f.flows[flowKey].lastSeen != tt.pkt.timestamp {
 				t.Errorf("got flow record last seen %v, want flow record last seen %v", f.flows[flowKey].lastSeen, tt.pkt.timestamp)
 			}
 		})
+	}
+}
+
+func TestFlushAll(t *testing.T) {
+	ctx := context.Background()
+	f := New()
+	wantFlows := make(map[FlowKey]*FlowRecord)
+	firstSrcAddr, _ := netip.AddrFromSlice(net.IP{0xc0, 0xa8, 0x01, 0x0a})
+	secondSrcAddr, _ := netip.AddrFromSlice(net.IP{0x0a, 0x00, 0x00, 0x05})
+	firstDstAddr, _ := netip.AddrFromSlice(net.IP{0x5d, 0xb8, 0xd8, 0x22})
+	secondDstAddr, _ := netip.AddrFromSlice(net.IP{0x5d, 0xb8, 0xd8, 0x22})
+	firstFlowKey := FlowKey{
+		srcIP:    firstSrcAddr,
+		dstIP:    firstDstAddr,
+		srcPort:  0xd431,
+		dstPort:  0x01bb,
+		protocol: 0x06,
+	}
+	secondFlowKey := FlowKey{
+		srcIP:    secondSrcAddr,
+		dstIP:    secondDstAddr,
+		srcPort:  0xd431,
+		dstPort:  0x01bb,
+		protocol: 0x06,
+	}
+	firstSeenTimestamp := time.Now().Add(-5 * time.Second)
+	secondSeenTimestamp := time.Now()
+	firstFlowRecord := FlowRecord{
+		FlowKey:     firstFlowKey,
+		byteCount:   38,
+		packetCount: 1,
+		firstSeen:   firstSeenTimestamp,
+		lastSeen:    firstSeenTimestamp,
+	}
+	secondFlowRecord := FlowRecord{
+		FlowKey:     secondFlowKey,
+		byteCount:   38,
+		packetCount: 1,
+		firstSeen:   secondSeenTimestamp,
+		lastSeen:    secondSeenTimestamp,
+	}
+
+	f.flows[firstFlowKey] = &firstFlowRecord
+	f.flows[secondFlowKey] = &secondFlowRecord
+	wantFlows[firstFlowKey] = &firstFlowRecord
+	wantFlows[secondFlowKey] = &secondFlowRecord
+
+	tests := []struct {
+		name      string
+		ctx       context.Context
+		wantFlows map[FlowKey]*FlowRecord
+	}{
+		{name: "valid", ctx: ctx, wantFlows: wantFlows},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			recordSlice := []FlowRecord{}
+			correctFlowsFlushed := map[FlowKey]bool{
+				firstFlowKey:  false,
+				secondFlowKey: false,
+			}
+
+			var wg sync.WaitGroup
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				f.flushAll(tt.ctx)
+			}()
+
+			for i := 0; i < len(tt.wantFlows); i++ {
+				select {
+				case record := <-f.Flushed():
+					recordSlice = append(recordSlice, record)
+				case <-time.After(time.Second):
+					t.Fatal("timed out waiting for flushed record")
+				}
+			}
+
+			wg.Wait()
+
+			for _, record := range recordSlice {
+				if tt.wantFlows[record.FlowKey] == nil {
+					t.Error("FlowKey does not exist in expected flows")
+					continue
+				}
+
+				if record.byteCount != tt.wantFlows[record.FlowKey].byteCount {
+					t.Errorf("got flow record byte count %#x, want flow record byte count %#x", record.byteCount, tt.wantFlows[record.FlowKey].byteCount)
+				}
+
+				if record.packetCount != tt.wantFlows[record.FlowKey].packetCount {
+					t.Errorf("got flow record packet count %#x, want flow record packet count %#x", record.packetCount, tt.wantFlows[record.FlowKey].packetCount)
+				}
+
+				if record.firstSeen != tt.wantFlows[record.FlowKey].firstSeen {
+					t.Errorf("got flow record first seen %v, want flow record first seen %v", record.firstSeen, tt.wantFlows[record.FlowKey].firstSeen)
+				}
+
+				if record.lastSeen != tt.wantFlows[record.FlowKey].lastSeen {
+					t.Errorf("got flow record last seen %v, want flow record last seen %v", record.lastSeen, tt.wantFlows[record.FlowKey].lastSeen)
+				}
+
+				if _, ok := tt.wantFlows[record.FlowKey]; ok {
+					correctFlowsFlushed[record.FlowKey] = true
+				}
+			}
+
+			for _, foundFlowKey := range correctFlowsFlushed {
+				if foundFlowKey == false {
+					t.Errorf("flow key never got flushed")
+				}
+			}
+
+			if len(f.flows) != 0 {
+				t.Errorf("expected length of flows to be 0, got %#x", len(f.flows))
+			}
+		})
+	}
+}
+
+func TestFlushAllCancelledCtx(t *testing.T) {
+	f := New()
+	srcAddr, _ := netip.AddrFromSlice(net.IP{0xc0, 0xa8, 0x01, 0x0a})
+	dstAddr, _ := netip.AddrFromSlice(net.IP{0x5d, 0xb8, 0xd8, 0x22})
+	flowKey := FlowKey{
+		srcIP:    srcAddr,
+		dstIP:    dstAddr,
+		srcPort:  0xd431,
+		dstPort:  0x01bb,
+		protocol: 0x06,
+	}
+	seen := time.Now()
+	flowRecord := FlowRecord{
+		FlowKey:     flowKey,
+		byteCount:   38,
+		packetCount: 1,
+		firstSeen:   seen,
+		lastSeen:    seen,
+	}
+
+	f.flows[flowKey] = &flowRecord
+
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	f.flushAll(ctx)
+
+	if len(f.flows) != 1 {
+		t.Errorf("expected length of flows to be 1, got %#x", len(f.flows))
 	}
 }
